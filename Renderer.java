3e23448811017b9b65487ff0a02e608cdf7ac85f
Jerrod P <41348542+JerrodP@users.noreply.github.com>
2018-11-26 12:08:44 -0600

    Added Stable Files
    Currently has speed control,
    score per clouds passed,
    and mostly improved methods and variables

---
+++ Renderer.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by acous on 8/23/2016.
+ */
+public class Renderer extends JPanel{
+
+    //private static final long serialVersionUID = 1L;
+
+    protected void paintComponent(Graphics g) {
+        Main.main.repaint(g);
+    }
+
+}
