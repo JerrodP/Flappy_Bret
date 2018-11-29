42f6faca96238f7c0637b24ac31fbb7b806aa4b4
Jerrod P <41348542+JerrodP@users.noreply.github.com>
2018-11-29 14:03:18 -0600

    Added drunkenness

--- Renderer.java
+++ Renderer.java
@@ -4,12 +4,14 @@ import java.awt.*;
 /**
  * Created by acous on 8/23/2016.
  */
+//public class Renderer {
 public class Renderer extends JPanel{
-
-    //private static final long serialVersionUID = 1L;
+	
+    private static final long serialVersionUID = 1L;
 
     protected void paintComponent(Graphics g) {
-        Main.main.repaint(g);
+      //  Flappy.instance.repaint(g);	since instance is public, do you need the Flappy.?
+    	Flappy.instance.repaint(g);
     }
 
 }
